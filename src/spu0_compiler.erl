883a4a328ad3ca5275d859ade20722fa48d6d193
Henry Nystrom <JanHenryNystrom@gmail.com>
2015-02-26 21:46:11 +0100

    Updated to support R17

--- src/spu0_compiler.erl
+++ src/spu0_compiler.erl
@@ -1,5 +1,5 @@
 %%==============================================================================
-%% Copyright 2013 Jan Henry Nystrom <JanHenryNystrom@gmail.com>
+%% Copyright 2013-2015 Jan Henry Nystrom <JanHenryNystrom@gmail.com>
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 %%% @end
 %%%
 %% @author Jan Henry Nystrom <JanHenryNystrom@gmail.com>
-%% @copyright (C) 2013, Jan Henry Nystrom <JanHenryNystrom@gmail.com>
+%% @copyright (C) 2013-2015, Jan Henry Nystrom <JanHenryNystrom@gmail.com>
 %%%-------------------------------------------------------------------
 -module(spu0_compiler).
 -copyright('Jan Henry Nystrom <JanHenryNystrom@gmail.com>').
@@ -37,19 +37,19 @@
 
 %% Records
 -record(parts, {attributes = []         :: [#attribute_p{}],
-                funcs      = dict:new() :: dict(),
+                funcs      = dict:new() :: dict:dict(),
                 exports    = []         :: [{atom(), integer()}]
                }).
 
 -record(lifting, {no      = 0 :: integer(),
-                  defs    = dict:new() :: dict(),
+                  defs    = dict:new() :: dict:dict(),
                   bound   = [] :: [integer()],
                   binding = [] :: [integer()],
                   errors  = [] :: [_]
                  }).
 
 -record(lifted, {attributes          :: [#attribute_p{}],
-                 funcs  = dict:new() :: dict(),
+                 funcs  = dict:new() :: dict:dict(),
                  exports             :: [{atom(), integer()}],
                  errors = []         :: [_]
                 }).
@@ -76,7 +76,7 @@
                    args           :: [_],
                    guard          :: [_],
                    body           :: [_],
-                   defs           :: dict()
+                   defs           :: dict:dict()
                   }).
 
 -record(func_c, {line         :: integer(),
